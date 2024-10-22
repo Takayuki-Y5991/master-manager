6bfa5dc42816bcd52a7145a15dd62524977352b1
konkon <konkon@debian.konkon>
2024-10-22 21:47:55 +0900

    :tada: add hours input components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import SidebarWithContentLayout from "./components/layout/Layout";
 import { Base } from "@/features/Base";
 import { menuItems } from "@/config/menus";
 import { Menus } from "./features/menus/Menus";
+import { HoursInputForm } from "./components/utils/HoursInputForm";
 
 function App() {
-  return <SidebarWithContentLayout children={Menus()} menus={menuItems} />;
+  return (
+    <SidebarWithContentLayout menus={menuItems}>
+      <HoursInputForm onSubmit={(data) => console.log(data)} />
+    </SidebarWithContentLayout>
+  );
 }
 
 export default App;
